0bf38b983568ca3f54d32772213b3988dde95256
Lina Renteria <linarente@gmail.com>
2020-08-17 16:58:15 +0200

    continuation_exo

--- Jour_4/01_doop.js
+++ Jour_4/01_doop.js
@@ -1,1 +1,15 @@
-console.log(process.argv)
+function doop(arg1, arg2, arg3) {
+
+}
+
+
+// function simpleAdding(num) {
+//     var total = 0;
+
+//     for(var i = 1;  i <= num; i++) {
+//         total += i;
+//     }
+//         return total ;
+// }
+
+// console.log(simpleAdding(140));
