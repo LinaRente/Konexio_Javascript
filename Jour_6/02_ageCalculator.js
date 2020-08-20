769c3adef18abdbc2ca9491400fd4759ccf26b48
lina <linarente@gmail.com>
2020-08-20 23:22:51 +0200

    add_date

---
+++ Jour_6/02_ageCalculator.js
@@ -0,0 +1,6 @@
+// Besoin d'une fonction qui a comme parametre date.
+//il me faut une variable qui me donne la date.
+// il me faut une variable pour stocker ma date de naissance
+// faire la date d'aujourd'hui moins la date de naissance. 
+
+
