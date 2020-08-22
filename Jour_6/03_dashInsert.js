2239e211e5cb51e774fcc985da42ca8794ee58a5
LinaRente <linarente@gmail.com>
2020-08-22 23:31:45 +0200

    day_6

--- Jour_6/03_dashInsert.js
+++ Jour_6/03_dashInsert.js
@@ -1,3 +1,22 @@
+// un split 
+// une boucle 
+// une condition
+// joint
+
+
 function dashInsert(str) { 
     
+    var string = str.toString().split("");
+    // console.log(string);
+
+    for(var i = 0; i < string.length; i++) {
+        if(string[i] % 2 == 1 && string[i+1]% 2 == 1){
+            string[i] = string[i] + " - "
+        }
+        // console.log(string);
+    }
+    return string.join("");
 }
+console.log(dashInsert('9999476'));
+
+
---
+++ Jour_6/06_longestWord.js
@@ -0,0 +1,16 @@
+function longestWord(str) { 
+
+    var string = str.split(" ");
+    var longest = 0;
+    var word;
+    
+    for (var i = 0; i < string.length; i++) {
+        
+        if (longest < string[i].length) {
+            longest = string[i].length;
+            word = string[i];
+        }
+    }
+    return word;
+}
+console.log(longestWord("I love dogs"))
