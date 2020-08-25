8e90703088de8629dde9e8397bf127480e1dd632
LinaRente <linarente@gmail.com>
2020-08-25 15:24:55 +0200

    day7

--- Jour_5/moreOrLess/index.js
+++ Jour_5/moreOrLess/index.js
@@ -1,18 +1,49 @@
-var prompt = require('prompt');
+var number = Math.floor(Math.random() * 100) + 1; // ordinateur
+function displayPrompt() {
 
-  //
-  // Start the prompt
-  //
-  prompt.start();
+    var prompt = require('prompt');
+    prompt.start();
+    //var number2 = parseInt(prompt);
+    // console.log('test 2', number)
+    prompt.get({ name: 'q', description: 'Do you like prompt?'}, function (err, result) {
+        //if ((prompt <= 100 && prompt >= 1)){
+        // console.log(result)
+        // console.log(parseInt(result.q))
+        if (number < parseInt(result.q)) {
+            console.log("C'est moins !")
+        }
+        else if (number > parseInt(result.q)) {
+            console.log("C'est plus !!")
+        } else if (parseInt(result.q) === number) {
+            console.log("Bravo, vous avez trouve le nombre mystère !!!");
+            return;
+        }
+        displayPrompt();
+        //return err;
+    });
+}
+displayPrompt();
 
-  //
-  // Get two properties from the user: username and email
-  //
-  prompt.get(['username', 'email'], function (err, result) {
-    //
-    // Log the results.
-    //
-    console.log('Command-line input received:');
-    console.log('  username: ' + result.username);
-    console.log('  email: ' + result.email);
-  });
+
+
+// function displayPrompt() {
+ 
+//     prompt.get({name: "guess", description: 'Devine le nombre mystere?' }, function (
+//     result
+//   ) {
+//     var mysteryNb = Math.floor(Math.random() * 100) + 1;
+//     console.log(mysteryNb)
+//     if (result.guess === mysteryNb || result.guess < mysteryNb ) {
+//       guess = parseInt(prompt.get('Cest plus petit'));
+//     } else if(resultat > mysteryNb) {
+//       guess = parseInt(prompt.get('Cest plus grand'));
+//     } else if(resultat.guess === guess) {
+//       guess = parseInt(prompt.get('bravo!'));
+//     } else {
+//       displayPrompt()
+//     }
+//   }));
+// }
+
+// displayPrompt()
+
--- Jour_5/test/index.js
+++ Jour_5/test/index.js
@@ -16,106 +16,19 @@
 // console.log(example.sayHelloInEnglish());
 
     
-//prompt
-
-// var prompt = require('prompt');
-
-// prompt.start();  // start the prompt
-
-// prompt.get(['username', 'email'], function (err, result) {  // get 2 properties username and email
-//   if (err) { return onErr(err); }
-//   console.log('Command-line input received:');
-//   console.log('  Username: ' + result.username);  // log results
-//   console.log('  Email: ' + result.email);
-// });
-
-// function onErr(err) {
-//   console.log(err);
-//   return 1;
-// }
-
-// var prompt = require('prompt');
-
-//   var properties = [
-//     {
-//       name: 'username',
-//       validator: /^[a-zA-Z\s\-]+$/,
-//       warning: 'Username must be only letters, spaces, or dashes'
-//     },
-//     {
-//       name: 'password',
-//       hidden: true;
-//     }
-//   ];
-
-//   prompt.start();
-
-//   prompt.get(properties, function (err, result) {
-//     if (err) { return onErr(err); }
-//     console.log('Command-line input received:');
-//     console.log('  Username: ' + result.username);
-//     console.log('  Password: ' + result.password);
-//   });
-
-//   function onErr(err) {
-//     console.log(err);
-//     return 1;
-//   }
-
-
-//Utiliser prompt pour la validation, les valeurs par défaut et plus
-
-
-// var prompt = require('prompt');
-
-// var schema = {
-//     properties: {
-//       name: {
-//         pattern: /^[a-zA-Z\s\-]+$/,
-//         message: 'Name must be only letters, spaces, or dashes',
-//         required: true
-//       },
-//       password: {
-//         hidden: true
-//       }
-//     }
-//   };
-
-//   //
-//   // Start the prompt
-//   //
-//   prompt.start();
-
-//   //
-//   // Get two properties from the user: email, password
-//   //
-//   prompt.get(schema, function (err, result) {
-//     //
-//     // Log the results.
-//     //
-//     console.log('Command-line input received:');
-//     console.log('  name: ' + result.name);
-//     console.log('  password: ' + result.password);
-//   });
-
-
-//Appeler prompt plusieurs fois
+// prompt
 
 var prompt = require('prompt');
 
-prompt.start();
+prompt.start();  // start the prompt
 
-function displayPrompt() {
-  prompt.get({ name: 'q', description: 'Do you like prompt?' }, function (
-    err,
-    result
-  ) {
-    if (result.q === 'y' || result.q === 'n') {
-      console.log('Answer received');
-    } else {
-      displayPrompt();
-    }
-  });
+prompt.get(['choisi un nombre'], function (number, result) {  // get 2 properties username and email
+  if (number != numberRandom ) { return onErr(err); }
+  console.log('Command-line input received:');
+
+});
+
+function onErr(err) {
+  console.log(err);
+  return 1;
 }
-
-displayPrompt();
