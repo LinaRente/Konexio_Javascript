8bfc67b9e00ee9f5104365c190bb1ebb577a807e
LinaRente <linarente@gmail.com>
2020-09-09 17:36:18 +0200

    blackjack - modif

--- Jour_8/blackjack/index.js
+++ Jour_8/blackjack/index.js
@@ -6,34 +6,32 @@ prompt.start();
 
 
 function displayPrompt() {
-    prompt.get({ name: 'q', description: 'Card ? (type "y" or "yes" for a new card)' }, function (err, result) {
+    prompt.get({ name: 'q', description: 'Card ? (type "y" or "yes" for a new card/ "p" or "pass" for end the game)' }, function (err, result) {
 
         var playerAnswer = result.q;
 
         if (playerAnswer === 'y' || playerAnswer === 'yes') {
             var randomCard = Math.floor(Math.random() * 10) + 1;
             playerScore = playerScore + randomCard;
-            console.log("Your score is " + playerScore + ", bank is " + bankScore);
+            // console.log('The bank had ' + bankScore);
+            console.log('Your Score is ' + playerScore)
         }
-        if (playerScore > 21) {
-            console.log('You Lose!')
+        if (playerScore > 21 || playerScore === bankScore) {
+            console.log('The bank had ' + bankScore + '!' + ' You Lose!')
+            return;
+        }
+        if (playerScore === 21) {
+            console.log('The bank had ' + bankScore + '!' + ' BLACK JACK !\nYou Win!');
             return;
         }
         if (playerAnswer === 'p' || playerAnswer === 'pass') {
-            if (playerScore === 21) {
-                console.log('BLACK JACK !\nYou Win!')
-                return;
-            }
+
             if (playerScore > bankScore) {
-                console.log('You Win!')
+                console.log('The bank had ' + bankScore + '!' + ' You Win!')
                 return;
             }
             if (playerScore < bankScore) {
-                console.log('You Lose');
-                return;
-            }
-            if (playerAnswer === bankScore) {
-                console.log('You Lose')
+                console.log('The bank had ' + bankScore + '!' + ' You Lose');
                 return;
             }
         }
