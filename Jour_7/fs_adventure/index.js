56ee0bce3504e864a2e4e099a0a267c571aefccc
LinaRente <linarente@gmail.com>
2020-09-09 12:28:49 +0200

    funcAdventure

--- Jour_7/fs_adventure/index.js
+++ Jour_7/fs_adventure/index.js
@@ -2,11 +2,14 @@ var fs = require('fs');
 var users = require('./users.json');
 
 // console.log(users);
-
-console.log(users[0].name)
-console.log(users[0].email)
-
-console.log("---")
-
-console.log(users[1].name)
-console.log(users[1].email)
+function fsAdventure () { 
+    
+    console.log(users[0].name)
+    console.log(users[0].email)
+    
+    console.log("---")
+    
+    console.log(users[1].name)
+    console.log(users[1].email)
+}
+fsAdventure();
--- Jour_8/favoriteMovies/index.js
+++ Jour_8/favoriteMovies/index.js
@@ -1,16 +1,44 @@
-var request = require('request');
+var request = require("request")
+var nombreDepart = 1;
+var nombreActuel = nombreDepart;
 
-request.get('http://www.omdbapi.com/?i=tt3896198&apikey=4c24e19', function (err, data, body) {
-    var json = JSON.parse(body);
-    // console.log(json.name)
+request.get('http://www.omdbapi.com/?t=titanic&plot=full&apikey=4c24e19', function (err, res, body) {
+  var json1 = JSON.parse(body);
 
-    if (err) {
-        console.log(err)
-        return
-    }
+  request.get('http://www.omdbapi.com/?t=violetta&plot=full&apikey=4c24e19', function (err, res, body) {
+    var json2 = JSON.parse(body);
+    
+    request.get("http://www.omdbapi.com/?t=twilight&plot=full&apikey=4c24e19", function (err, res, body) {
+      var json3 = JSON.parse(body);
 
-    for (var i = 0; i <= json.length - 1; i++) {
-        console.log('--')
-        console.log(json[i].name)
-    }
+      request.get("http://www.omdbapi.com/?t=avatar&plot=full&apikey=4c24e19", function (err, res, body) {
+        var json4 = JSON.parse(body);
+
+        console.log(nombreActuel++ + ".")
+        console.log("Title :" + " " + json1.Title)
+        console.log("Year :" + " " + json1.Year)
+        console.log("Genre :" + " " + json1.Genre)
+        console.log("Actors :" + " " + json1.Actors)
+        console.log(nombreActuel++ + ".")
+
+        console.log("Title :" + " " + json2.Title)
+        console.log("Year :" + " " + json2.Year)
+        console.log("Genre :" + " " + json2.Genre)
+        console.log("Actors :" + " " + json2.Actors)
+        console.log(nombreActuel++ + ".")
+
+        console.log("Title :" + " " + json3.Title)
+        console.log("Year :" + " " + json3.Year)
+        console.log("Genre :" + " " + json3.Genre)
+        console.log("Actors :" + " " + json3.Actors)
+        console.log(nombreActuel++ + ".")
+
+        console.log("Title :" + " " + json4.Title)
+        console.log("Year :" + " " + json4.Year)
+        console.log("Genre :" + " " + json4.Genre)
+        console.log("Actors :" + " " + json4.Actors)
+        
+      });
+    });
+  });
 })
