791eb1139c6b7f806eedecf9b7260f7cb0b7ec9e
LinaRente <linarente@gmail.com>
2020-09-11 16:44:43 +0200

    currency

--- Jour_8/currencyConverter/index.js
+++ Jour_8/currencyConverter/index.js
@@ -1,18 +1,33 @@
-var request = require('request');
-request.get("http://data.fixer.io/api/latest?access_key=b3bf580f476dd1fa3e6d1ca802615008", function (err, res, body) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    var json = JSON.parse(body);
-    var arg1 = process.argv[2];
-    var arg2 = process.argv[3];
-    var arg3 = parseInt(process.argv[4]);
-    var argument1 = arg1.toLowerCase();
-    var argument2 = arg2.toLowerCase();
-    argument1 = json.rates.EUR;
-    argument2 = json.rates.USD;
-    var result = arg3 * argument1
-    var rest = result * argument2
-    console.log(rest)
+var request = require("request")
+
+request.get("http://data.fixer.io/api/latest?access_key=b3bf580f476dd1fa3e6d1ca802615008", function (err, data, body) {
+
+
+    function convertiseur(arg1, arg2, arg3) {
+        var arg1 = (process.argv[2]);
+        var arg2 = (process.argv[3]);
+
+        var arg = arg1.toUpperCase()
+
+        var args = arg2.toUpperCase();
+        var arg3 = parseInt(process.argv[4]);
+
+        var json = JSON.parse(body)
+
+        // console.log("monnaie", arg)
+        // console.log("json rates", json.rates[arg])
+        // console.log("json rates", json.rates[args])
+
+        var currency1 = json.rates[arg]
+        currency2 = json.rates[args]
+        if (currency1 && currency2) {
+            var result = currency1 * arg3 // arg1 EUR=1   * 35
+            var result2 = result * currency2 // arg2 USD = 1.183865 * res(35)
+            console.log(result2)
+            return
+        }else if (currency1 !== json.rates || currency2 !== json.rates) { 
+            console.log('Error')
+            return
+        }
+    } convertiseur()
 })
